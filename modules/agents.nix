21f71bccde6cceb36a7eb239a93b16c75e072509
Zheng He Hu <aeonzh@hotmail.com>
2026-05-11 03:24:14 +0200

    feat: add pi

--- modules/agents.nix
+++ modules/agents.nix
@@ -3,71 +3,10 @@
   # AI Agents and autonomous coding tools
   home.packages = with pkgs; [
     github-copilot-cli
+    pi-coding-agent
   ];
 
-  programs.gemini-cli = {
-    enable = true;
-    settings = {
-      theme = "Default";
-      selectedAuthType = "oauth-personal";
-      checkpointing = true;
-      experimental = {
-        enableAgents = true;
-      };
-      security.auth.selectedType = "oauth-personal";
-      ui = {
-        footer = {
-          items = [
-            "workspace"
-            "git-branch"
-            "sandbox"
-            "model-name"
-            "quota"
-            "context-used"
-            "code-changes"
-            "token-count"
-          ];
-        };
-        theme = "Default";
-      };
-    };
-  };
-
-  programs.opencode = {
-    enable = true;
-    settings = {
-      provider = {
-        openai = {
-          options = {
-            reasoningEffort = "medium";
-            reasoningSummary = "auto";
-            textVerbosity = "medium";
-            include = [ "reasoning.encrypted_content" ];
-            store = false;
-          };
-        };
-      };
-    };
-  };
-
-  programs.codex = {
-    enable = true;
-    settings = {
-      personality = "pragmatic";
-      model = "gpt-5.3-codex";
-      model_reasoning_effort = "medium";
-
-      plugins."github@openai-curated".enabled = true;
-
-      tui.status_line = [
-        "model-with-reasoning"
-        "current-dir"
-        "git-branch"
-        "context-usage"
-        "five-hour-limit"
-        "weekly-limit"
-        "context-window-size"
-      ];
-    };
-  };
+  programs.gemini-cli.enable = true;
+  programs.opencode.enable = true;
+  programs.codex.enable = true;
 }
